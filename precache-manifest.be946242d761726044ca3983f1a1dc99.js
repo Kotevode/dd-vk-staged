0cf956d88c4b4667eb1a24bff9bc42678b0b7453
Mark <kotevode@icloud.com>
2019-01-10 21:40:13 +0500

    Updates

---
+++ precache-manifest.be946242d761726044ca3983f1a1dc99.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ca79381c88a1278bf7518a0f94aa64a",
+    "url": "/ddvk/staged/static/media/Logo.5ca79381.svg"
+  },
+  {
+    "revision": "40ccec13752ce1f19ec1b0658906aace",
+    "url": "/ddvk/staged/static/media/Icon14Statoscope.40ccec13.svg"
+  },
+  {
+    "revision": "ceb67ee6f45d9665faf8781a3730f6e1",
+    "url": "/ddvk/staged/static/media/Icon14Pin.ceb67ee6.svg"
+  },
+  {
+    "revision": "e8eeca859aeda5b1ab529f4ddebe6670",
+    "url": "/ddvk/staged/static/media/Icon14Calendar.e8eeca85.svg"
+  },
+  {
+    "revision": "b4fad95798401d897363",
+    "url": "/ddvk/staged/static/js/runtime~main.b4fad957.js"
+  },
+  {
+    "revision": "05ecbdb97d9990c7b557",
+    "url": "/ddvk/staged/static/js/main.05ecbdb9.chunk.js"
+  },
+  {
+    "revision": "c55fd1caa09100aef852",
+    "url": "/ddvk/staged/static/js/1.c55fd1ca.chunk.js"
+  },
+  {
+    "revision": "05ecbdb97d9990c7b557",
+    "url": "/ddvk/staged/static/css/main.4ab35eeb.chunk.css"
+  },
+  {
+    "revision": "c55fd1caa09100aef852",
+    "url": "/ddvk/staged/static/css/1.35c65066.chunk.css"
+  },
+  {
+    "revision": "b79dbdad89c3a4390cd5658ac3232352",
+    "url": "/ddvk/staged/index.html"
+  }
+];
