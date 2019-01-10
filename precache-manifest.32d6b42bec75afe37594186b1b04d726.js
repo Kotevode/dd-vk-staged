9d99e8a647c0efbd6b9ad489d9a7623c35cd781c
Mark <kotevode@icloud.com>
2019-01-10 21:51:49 +0500

    Updates

---
+++ precache-manifest.32d6b42bec75afe37594186b1b04d726.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ca79381c88a1278bf7518a0f94aa64a",
+    "url": "/dd-vk-staged/static/media/Logo.5ca79381.svg"
+  },
+  {
+    "revision": "40ccec13752ce1f19ec1b0658906aace",
+    "url": "/dd-vk-staged/static/media/Icon14Statoscope.40ccec13.svg"
+  },
+  {
+    "revision": "ceb67ee6f45d9665faf8781a3730f6e1",
+    "url": "/dd-vk-staged/static/media/Icon14Pin.ceb67ee6.svg"
+  },
+  {
+    "revision": "e8eeca859aeda5b1ab529f4ddebe6670",
+    "url": "/dd-vk-staged/static/media/Icon14Calendar.e8eeca85.svg"
+  },
+  {
+    "revision": "edb71fe9f63c343f5e72",
+    "url": "/dd-vk-staged/static/js/runtime~main.edb71fe9.js"
+  },
+  {
+    "revision": "05ecbdb97d9990c7b557",
+    "url": "/dd-vk-staged/static/js/main.05ecbdb9.chunk.js"
+  },
+  {
+    "revision": "c55fd1caa09100aef852",
+    "url": "/dd-vk-staged/static/js/1.c55fd1ca.chunk.js"
+  },
+  {
+    "revision": "05ecbdb97d9990c7b557",
+    "url": "/dd-vk-staged/static/css/main.4ab35eeb.chunk.css"
+  },
+  {
+    "revision": "c55fd1caa09100aef852",
+    "url": "/dd-vk-staged/static/css/1.35c65066.chunk.css"
+  },
+  {
+    "revision": "4ea8119d8ac6cda12d6f535a4d6ec888",
+    "url": "/dd-vk-staged/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ddvk/staged/precache-manifest.be946242d761726044ca3983f1a1dc99.js"
+  "/dd-vk-staged/precache-manifest.32d6b42bec75afe37594186b1b04d726.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ddvk/staged/index.html", {
+workbox.routing.registerNavigationRoute("/dd-vk-staged/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
