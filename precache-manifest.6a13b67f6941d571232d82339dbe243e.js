66725803058af6250af5a87cee2ede44551a0221
Mark <kotevode@icloud.com>
2019-01-27 18:52:45 +0500

    Updates

--- precache-manifest.6a13b67f6941d571232d82339dbe243e.js
+++ precache-manifest.6a13b67f6941d571232d82339dbe243e.js
@@ -4,16 +4,16 @@ self.__precacheManifest = [
     "url": "./static/media/Icon14Pin.ceb67ee6.svg"
   },
   {
-    "revision": "360854361cf804ba9c1b",
-    "url": "./static/css/main.ffb8cbd6.chunk.css"
+    "revision": "eca8acda64b37d285e71",
+    "url": "./static/css/main.29897983.chunk.css"
   },
   {
     "revision": "e5a45960f96b317e7215490043453288",
     "url": "./static/media/OrderSuccessCheckmark.e5a45960.svg"
   },
   {
-    "revision": "2c288042fb7e08491f19",
-    "url": "./static/js/1.2c288042.chunk.js"
+    "revision": "ebcb68c47b85cdd906e4",
+    "url": "./static/js/1.ebcb68c4.chunk.js"
   },
   {
     "revision": "4a686d48d5a089750c49",
@@ -28,8 +28,8 @@ self.__precacheManifest = [
     "url": "./static/media/Icon14Statoscope.40ccec13.svg"
   },
   {
-    "revision": "360854361cf804ba9c1b",
-    "url": "./static/js/main.36085436.chunk.js"
+    "revision": "eca8acda64b37d285e71",
+    "url": "./static/js/main.eca8acda.chunk.js"
   },
   {
     "revision": "e8eeca859aeda5b1ab529f4ddebe6670",
@@ -48,11 +48,11 @@ self.__precacheManifest = [
     "url": "./static/media/Icon13Pedestrian.763bf14d.svg"
   },
   {
-    "revision": "2c288042fb7e08491f19",
+    "revision": "ebcb68c47b85cdd906e4",
     "url": "./static/css/1.16986dbf.chunk.css"
   },
   {
-    "revision": "77932bde699157c741688c62abfcbdc7",
+    "revision": "f0a3274bae3d6370b63b9728bdc95bf3",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c405ddc655c694ae041e5fe57a694576.js"
+  "./precache-manifest.6a13b67f6941d571232d82339dbe243e.js"
 );
 
 workbox.clientsClaim();
