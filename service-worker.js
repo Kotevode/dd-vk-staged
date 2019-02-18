421ef7d19d8d5dcaa7b9395bb2b183004270c298
Mark <kotevode@icloud.com>
2019-02-18 23:48:44 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ccf7896ceccb289b90f1592a5f070e28.js"
+  "./precache-manifest.6d6cc758ebc766da4d1cfe51be935db9.js"
 );
 
 workbox.clientsClaim();
